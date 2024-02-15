39c00df2c095b8366e2c158f7885d099897756d9
ZerinEmin <93584957+ZerinEmin@users.noreply.github.com>
2024-02-15 13:50:20 +0100

    vowels presence

---
+++ exerciseprojects/src/exerciseprojects/VowelPresence.java
@@ -0,0 +1,17 @@
+package exerciseprojects;
+
+import java.util.Scanner;
+
+public class VowelPresence {
+	static void Vowel_Or_Consonant(char y) 
+	
+    { 
+		
+        if (y == 'a' || y == 'e' || y == 'i' || y == 'o'
+            || y == 'u') 
+            System.out.println("It is a Vowel."); 
+        else
+            System.out.println("It is a Consonant."); 
+    } 
+
+}
